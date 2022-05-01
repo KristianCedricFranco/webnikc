8776781195349fdd1d617431cab42fce35fa43b0
oOZeroXx <104554822+oOZeroXx@users.noreply.github.com>
2022-05-01 23:14:28 +0800

    Add files via upload

---
+++ Another.php
@@ -0,0 +1,199 @@
+<html>
+<head>
+<title>Franco's Another Website</title>
+</head>
+<style>
+body {
+  background-image: url('./ImgForWeb/html-color-codes-color-tutorials-hero.jpg');
+  background-repeat: no-repeat;
+  background-attachment: scroll;
+  background-size:cover;
+
+}
+
+a:link, a:visited {
+  background-color: white;
+  color: black;
+  border: 2px solid blue;
+  padding: 10px 20px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+}
+
+a:hover, a:active {
+  background-color: royalblue;
+  color: white;
+}
+
+.justify{
+    text-align: justify;
+}
+
+.heading{
+  background-color: white;
+  border: 2px solid black;
+  outline: #A4DE02 solid 10px;
+  margin: auto;  
+  padding: 20px;
+  text-align: center;
+  color: black;
+  font-size: 500%;
+}
+
+.h2{
+  color: yellow;
+  font-size: 400%;
+  text-align: center;
+
+
+}
+
+.h1{
+    text-align: center;
+}
+
+.p{
+  color: orange;
+  font-family:FreeMono, monospace;
+  font-size:160%;"
+  text-align: justify;
+}
+
+.title{
+  color: #5affde;
+  font-family: "Lucida Console", Courier, monospace;
+  font-size:200%;"
+}
+
+table {
+  font-family: arial, sans-serif;
+  border-collapse: collapse;
+  width: 50%;
+}
+
+td, th {
+  border: 1px solid #BEBEBE;
+  text-align: left;
+  padding: 8px;
+  width: 50%;
+}
+
+tr:nth-child(even) {
+  background-color: #808080;
+  width: 50%;
+}
+</style>
+<br>
+<h1 class= "heading h1"><center>Welcome to my Another Website</center></h1>
+<br><br><br><br>
+<p1 class= "h2"><center>Feel free to make yourself at home</center></p1>
+<br><br><br><br>
+<img src="./ImgForWeb/157052939_4124240810996217_2251996699964996503_n.jpg" alt="Me"style="width:400px;height:400px;" align="right" hspace="50" vspace="25">
+
+<h2 class="title">Introduction:</h2>
+<p2 class="p">
+  Hello there! <mark>Kristian Cedric Y. Franco</mark> is my name. I am 20 years old and I live in La Carlota City's Gan Subdivision. I graduated from <u>Doña Hortencia Salas Benedicto National High School <i>(DHSBNHS)</i></u> and <u>La Carlota South Elementary School-1 <i>(LCSES-1)</i></u>. My father's name is Jesus L. Franco, and my mother's name is Maria Purita Y. Franco. I have two brothers, JV Derick Y. Franco and Luis Jerick Y. Franco. My hobbies include playing video games, reading manga /manhwa/ webtoon, eating, sleeping, going to new places, and watching anime. My ambition is to become a programmer while also living a pleasant life.
+<br><br><br><br></p2>
+<center><table style="color: white;">
+  <tr>
+    <th colspan="2"><center><b>Physical Attributes<b></center></th>
+  </tr>
+  <tr>
+    <td><b>Hair</b></td>
+    <td>Curly Black</td>
+  </tr>
+  <tr>
+    <td><b>Eyes</b></td>
+    <td>Narrow Eyes</td>
+  </tr>
+  <tr>
+    <td><b>Nose</b></td>
+    <td>Pango</td>
+  </tr>
+  <tr>
+    <td><b>Height</b></td>
+    <td>167.64 cm.</td>
+  </tr>
+  <tr>
+    <td><b>Weight</b></td>
+    <td>75 kg. kapag busog</td>
+  </tr>
+</table></center>
+<br><br><br><br><br><br><br><br>
+<center><table style="color: white;">
+  <tr>
+    <th colspan="2"><center><b>Hobbies<b></center></th>
+  </tr>
+  <tr>
+    <td><b>#1</b></td>
+    <td>Reading manga/manhwa/webtoon</td>
+  </tr>
+  <tr>
+    <td><b>#2</b></td>
+    <td>Watching anime or movies</td>
+  </tr>
+  <tr>
+    <td><b>#3</b></td>
+    <td>Going to new places</td>
+  </tr>
+  <tr>
+    <td><b>#4</b></td>
+    <td>Eating Foods</td>
+  </tr>
+  <tr>
+    <td><b>#5</b></td>
+    <td>Sleeping</td>
+  </tr>
+</table></center>
+<br><br><br><br><br><br><br><br>
+<center><table style="color: black;">
+  <tr>
+    <th colspan="2"><center><b>Personal Info<b></center></th>
+  </tr>
+  <tr>
+    <td><b>Name</b></td>
+    <td>Kristian Cedric Y. Franco</td>
+  </tr>
+  <tr>
+    <td><b>Age</b></td>
+    <td>20 years old</td>
+  </tr>
+  <tr>
+    <td><b>Address</b></td>
+    <td>Gan Subdivision, La Carlota City</td>
+  </tr>
+  <tr>
+    <td><b>Blood Type</b></td>
+    <td>AB</td>
+  </tr>
+  <tr>
+    <td><b>Course</b></td>
+    <td>Bachelor of Science in Information Technology <i>(BSIT)</i></td>
+  <tr>
+    <td><b>Citizenship</b></td>
+    <td>Filipino</td>
+  </tr>
+    <tr>
+    <td><b>Civil Status</b></td>
+    <td>Single</td>
+  </tr>
+  </tr>
+  <tr>
+    <td><b>Father</b></td>
+    <td>Jesus L. Franco</td>
+  </tr>
+  <tr>
+    <td><b>Mother</b></td>
+    <td>Maria Purita Y. Franco</td>
+  </tr>
+  <tr>
+    <td><b>Siblings</b></td>
+    <td>JV Derick Y. Franco<br> Luis Jerick Y. Franco</td>
+  </tr>
+</table></center>
+<br><br>
+<center><a href="https://francowebsite.000webhostapp.com/" target="_blank">Click here for another website</a></center>
+<br><br><br><br><br>
+</body>
+</html>
